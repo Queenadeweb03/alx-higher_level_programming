d1e9a481c4746b89637f99b3dbf2c6dd2b7279b8
Queenadeweb03 <adelekeadeola1430@gmail.com>
2022-09-25 01:24:14 -0700

    hot fix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
+from add_0 import add
 
+if __name__ == '__main__':
     a = 1
-    a = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
